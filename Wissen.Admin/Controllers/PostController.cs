f09280c64725695a8ddae7b03ab11cf7551bf2a3
TanjuSimsek <tanju.simsek@yandex.com>
2019-02-19 11:08:15 +0300

    AlLUpdated

--- Wissen.Admin/Controllers/PostController.cs
+++ Wissen.Admin/Controllers/PostController.cs
@@ -12,7 +12,8 @@ namespace Wissen.Admin.Controllers
     {
         private readonly IPostService postService;
         private readonly ICategoryService categoryService;
-        public PostController(IPostService postService, ICategoryService categoryService) {
+        public PostController(IPostService postService, ICategoryService categoryService)
+        {
             this.postService = postService;
             this.categoryService = categoryService;
 
@@ -28,8 +29,73 @@ namespace Wissen.Admin.Controllers
         public ActionResult Craate()
         {
             var post = new Post();
+            ViewBag.CategoryId = new SelectList(categoryService.GetAll(), "Id", "Name");
             return View(post);
 
         }
+        [HttpPost]
+        public ActionResult Craate(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                postService.Insert(post);
+                
+                return RedirectToAction("Index");
+
+
+            }
+            ViewBag.CategoryId = new SelectList(categoryService.GetAll(), "Id", "Name",post.CategoryId);
+            return View();
+
+
+        }
+        public ActionResult Edit(int id)
+        {
+
+            var post = postService.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+
+            }
+            ViewBag.CategoryId = new SelectList(categoryService.GetAll(), "Id", "Name", post.CategoryId);
+            return View(post);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                var model = postService.Find(post.Id);
+                model.Title = post.Title;
+                model.Description = post.Description;
+                model.CategoryId = post.CategoryId;
+                postService.Update(model);
+                return RedirectToAction("Index");
+
+
+            }
+            ViewBag.CategoryId = new SelectList(categoryService.GetAll(), "Id", "Name", post.CategoryId);
+            return View();
+
+
+        }
+        public ActionResult Delete(int id)
+        {
+
+
+
+            postService.Delete(id);
+            return RedirectToAction("Index");
+
+
+
+
+
+
+        }
+
+
     }
 }
